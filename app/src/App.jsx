588956071c3a74406c8e057f8512a8a22d035813
Kykal <benavides.alan@hotmail.com>
2021-11-27 22:54:19 -0600

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Se importan hooks de Material-UI
+import { Grid } from '@mui/material';
+
+//Contenido del componente
+const App = () => {
+
+   return (
+      <>
+         <Grid>
+         
+         </Grid>  
+      </>
+   );
+};
+
+export default App; //Se exporta el componente
