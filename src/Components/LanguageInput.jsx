d58efa3adccaa31d654968fffbc52af4267b1291
Kykal <benavides.alan@hotmail.com>
2021-11-29 09:01:14 -0600

    Finished?
    Full functional

---
+++ src/Components/LanguageInput.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+//Import Material-UI hooks
+import { Autocomplete, TextField } from '@mui/material';
+
+//Component content
+const LanguageInput = (props) => {
+
+   const options = [
+      'Python',
+      'MongoDB'
+   ]
+
+   const [visualValue, setVisualValue] = useState(''); //inputValue - Visual only
+   const [value, setValue] = useState(null); //Value - real value
+
+   const inputHandler = (event, newValue) => {
+      setValue(newValue);
+      props.onChange(newValue)
+   };
+
+   return (
+      <Autocomplete
+         value={value}
+         onChange={inputHandler}
+
+         inputValue={visualValue}
+         onInputChange={(event, newValue) => {
+            setVisualValue(newValue);
+         }}
+
+         options={options}
+
+         renderInput={(params) => (
+            <TextField
+               variant='standard'
+               {...params}
+               label='Technology'
+               required
+               helperText='PL, framework, library, etc.'
+            />
+         )}
+      />
+   );
+};
+
+export default LanguageInput; //Export component
